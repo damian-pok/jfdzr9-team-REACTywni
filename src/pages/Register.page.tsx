dd126c424f0d7becf27cbbe1ec0f6d4e99fbd638
Damian Pokrop <d.pokrop@gmail.com>
2023-03-21 12:26:17 +0100

    initial commit: firebase db created, .env file created, react-hook-form library installed

--- src/pages/Register.page.tsx
+++ src/pages/Register.page.tsx
@@ -1,4 +1,19 @@
+//import libraries
+import { useForm } from "react-hook-form";
+
+//import styles
+//...
+
+//types and interfaces
+interface IForm {
+  email: string;
+  password: string;
+}
+
+//function
 const Register = () => {
+  const { control, handleSubmit } = useForm<IForm>();
+
   return <h1>Rejestracja!</h1>;
 };
 
