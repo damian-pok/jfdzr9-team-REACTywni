db66bc758708ba749668cfdb011189ac97744697
Katarzyna Chmielewska <katarzynachmielewska@int.pl>
2023-03-23 18:01:15 +0100

    finish task and menu

---
+++ src/components/Menu/Menu.styled.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const MenuField = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  box-sizing: content-box;
+  padding: 20px 30px;
+  height: 40px;
+  background-color: var(--color-white);
+  border-bottom: 1px solid var(--light-grey-color);
+`;
+
+export const ButtonMenuField = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: end;
+  align-items: center;
+  gap: 20px;
+`;
