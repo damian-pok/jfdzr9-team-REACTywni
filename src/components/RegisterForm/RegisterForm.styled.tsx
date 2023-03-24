2927b1174de6cda41952ab86649fbb23120ccc66
Damian Pokrop <d.pokrop@gmail.com>
2023-03-24 19:50:13 +0100

    Add user basic funcionality created

--- src/components/RegisterForm/RegisterForm.styled.tsx
+++ src/components/RegisterForm/RegisterForm.styled.tsx
@@ -1,1 +1,13 @@
 import styled from "styled-components";
+
+export const RegisterFormStyled = styled.form`
+  max-width: 640px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  background-color: lightyellow;
+  border-radius: 5px;
+  padding: 4rem;
+`;
--- src/pages/Register.page.tsx
+++ src/pages/Register.page.tsx
@@ -1,8 +1,14 @@
 //import libraries
-import { useForm } from "react-hook-form";
-
+import { useForm, Controller, SubmitHandler } from "react-hook-form";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { FirebaseError } from "@firebase/util";
+import { useState } from "react";
 //import styles
-//...
+import { RegisterFormStyled } from "../components/RegisterForm/RegisterForm.styled";
+
+//firebase config files
+import { firebaseErrors } from "../firebase/firebase.errors";
+import { auth } from "../firebase/firebase.config";
 
 //types and interfaces
 interface IForm {
@@ -10,11 +16,38 @@ interface IForm {
   password: string;
 }
 
+type FirebaseErrorsKeys = keyof typeof firebaseErrors;
+
 //function
 const Register = () => {
   const { control, handleSubmit } = useForm<IForm>();
+  const [error, setError] = useState("");
 
-  return <h1>Rejestracja!</h1>;
+  const onSubmit: SubmitHandler<IForm> = ({ email, password }) => {
+    createUserWithEmailAndPassword(auth, email, password).catch((error: FirebaseError) => {
+      setError(firebaseErrors[error.code as FirebaseErrorsKeys]);
+    });
+  };
+
+  return (
+    <>
+      <h1>Rejestracja!</h1>
+      <RegisterFormStyled onSubmit={handleSubmit(onSubmit)}>
+        <Controller
+          name="email"
+          control={control}
+          render={({ field }) => <input placeholder="Type email" type={"email"} {...field} />}
+        />
+        <Controller
+          name="password"
+          control={control}
+          render={({ field }) => <input placeholder="Type password" type={"password"} {...field} />}
+        />
+        <button type="submit">Zarejestruj!</button>
+        {error}
+      </RegisterFormStyled>
+    </>
+  );
 };
 
 export default Register;
