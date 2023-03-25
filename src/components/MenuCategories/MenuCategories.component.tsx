3d162903a9c31c3bfca970e5bae1a2733e39b6d0
Marta Zielińska <marta.zielinska1987@gmail.com>
2023-03-25 11:20:34 +0100

    In component MenuCategories.component.tsx buttons added. Component imported to HowClient.page.tsx

---
+++ src/components/MenuCategories/MenuCategories.component.tsx
@@ -0,0 +1,12 @@
+export const MenuCategories = () => {
+  return (
+    <div>
+      <button>Branding</button>
+      <button>Druk</button>
+      <button>Digital</button>
+      <button>UX/UI</button>
+      <button>Ilustracje</button>
+      <button>Inne</button>
+    </div>
+  );
+};
