834d8e98ee05d7272f6bda02ae148243ef83f470
Marta Zielińska <marta.zielinska1987@gmail.com>
2023-03-25 12:48:19 +0100

    Const MenuButton created and styled. SecondaryButton in MenuCategories replaced with MenuButton

--- src/components/MenuCategories/MenuCategories.component.tsx
+++ src/components/MenuCategories/MenuCategories.component.tsx
@@ -1,15 +1,15 @@
-import { SecondaryButton } from "../UI/Buttons/Buttons.styled";
+import { MenuButton } from "./MenuCategories.styled";
 import { MenuCategoriesButtons } from "./MenuCategories.styled";
 
 export const MenuCategories = () => {
   return (
     <MenuCategoriesButtons>
-      <SecondaryButton>Branding</SecondaryButton>
-      <SecondaryButton>Druk</SecondaryButton>
-      <SecondaryButton>Digital</SecondaryButton>
-      <SecondaryButton>UX/UI</SecondaryButton>
-      <SecondaryButton>Ilustracje</SecondaryButton>
-      <SecondaryButton>Inne</SecondaryButton>
+      <MenuButton>Branding</MenuButton>
+      <MenuButton>Druk</MenuButton>
+      <MenuButton>Digital</MenuButton>
+      <MenuButton>UX/UI</MenuButton>
+      <MenuButton>Ilustracje</MenuButton>
+      <MenuButton>Inne</MenuButton>
     </MenuCategoriesButtons>
   );
 };
--- src/components/MenuCategories/MenuCategories.styled.ts
+++ src/components/MenuCategories/MenuCategories.styled.ts
@@ -8,3 +8,21 @@ export const MenuCategoriesButtons = styled.div`
   gap: 20px;
   padding: 45px;
 `;
+
+export const MenuButton = styled.button`
+  width: fit-content;
+  height: 40px;
+  padding: 20px 40px;
+  font-weight: bold;
+  color: var(--dark-grey-color);
+  background-color: var(--white-color);
+  border: none;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  :hover {
+    color: var(--white-color);
+    background-color: var(--blue-grey-color);
+  }
+`;
