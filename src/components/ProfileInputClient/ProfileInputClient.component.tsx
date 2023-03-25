999a1a93fb2553e6bd34eae723c5a0da83ed3707
Damian Pokrop <d.pokrop@gmail.com>
2023-03-25 13:05:15 +0100

    basic profile forms styled

--- src/components/ProfileInputClient/ProfileInputClient.component.tsx
+++ src/components/ProfileInputClient/ProfileInputClient.component.tsx
@@ -1,6 +1,9 @@
 //libraries
 import { Controller, useForm } from "react-hook-form";
 
+//components
+import { ProfileInputClientStyled } from "./ProfileInputClient.styled";
+
 //types
 interface IProfileInputClient {
   company: string;
@@ -15,37 +18,39 @@ export const ProfileInputClient = () => {
   const { control } = useForm<IProfileInputClient>();
   return (
     <>
-      <Controller
-        name="company"
-        control={control}
-        render={({ field }) => <input placeholder="Nazwa firmy" type={"text"} {...field} />}
-      />
-      <Controller
-        name="email"
-        control={control}
-        render={({ field }) => <input placeholder="Email" type={"email"} {...field} />}
-      />
-      <Controller
-        name="nip"
-        control={control}
-        render={({ field }) => <input placeholder="NIP" type={"number"} {...field} />}
-      />
-      <Controller
-        name="country"
-        control={control}
-        render={({ field }) => <input placeholder="Kraj" type={"text"} {...field} />}
-      />
-      <Controller
-        name="city"
-        control={control}
-        render={({ field }) => <input placeholder="Miasto" type={"text"} {...field} />}
-      />
-      <Controller
-        name="street"
-        control={control}
-        render={({ field }) => <input placeholder="Ulica i numer" type={"text"} {...field} />}
-      />
-      <button>Send</button>
+      <ProfileInputClientStyled>
+        <Controller
+          name="company"
+          control={control}
+          render={({ field }) => <input placeholder="Nazwa firmy" type={"text"} {...field} />}
+        />
+        <Controller
+          name="email"
+          control={control}
+          render={({ field }) => <input placeholder="Email" type={"email"} {...field} />}
+        />
+        <Controller
+          name="nip"
+          control={control}
+          render={({ field }) => <input placeholder="NIP" type={"number"} {...field} />}
+        />
+        <Controller
+          name="country"
+          control={control}
+          render={({ field }) => <input placeholder="Kraj" type={"text"} {...field} />}
+        />
+        <Controller
+          name="city"
+          control={control}
+          render={({ field }) => <input placeholder="Miasto" type={"text"} {...field} />}
+        />
+        <Controller
+          name="street"
+          control={control}
+          render={({ field }) => <input placeholder="Ulica i numer" type={"text"} {...field} />}
+        />
+        <button>Send</button>
+      </ProfileInputClientStyled>
     </>
   );
 };
--- src/components/ProfileInputFreelancer/ProfileInputFreelancer.component.tsx
+++ src/components/ProfileInputFreelancer/ProfileInputFreelancer.component.tsx
@@ -1,5 +1,6 @@
 //libraries
 import { Controller, useForm } from "react-hook-form";
+import { AboutMeStyled, ProfileInputFreelancerStyled } from "./ProfileInputFreelancer.styled";
 
 //types
 interface IProfileInputFreelancer {
@@ -18,53 +19,55 @@ export const ProfileInputFreelancer = () => {
   const { control } = useForm<IProfileInputFreelancer>();
   return (
     <>
-      <Controller
-        name="firstName"
-        control={control}
-        render={({ field }) => <input placeholder="Imię" type={"text"} {...field} />}
-      />
-      <Controller
-        name="secondName"
-        control={control}
-        render={({ field }) => <input placeholder="Nazwisko" type={"text"} {...field} />}
-      />
-      <Controller
-        name="email"
-        control={control}
-        render={({ field }) => <input placeholder="Email" type={"email"} {...field} />}
-      />
-      <Controller
-        name="country"
-        control={control}
-        render={({ field }) => <input placeholder="Kraj" type={"text"} {...field} />}
-      />
-      <Controller
-        name="city"
-        control={control}
-        render={({ field }) => <input placeholder="Miasto" type={"text"} {...field} />}
-      />
-      <Controller
-        name="experience"
-        control={control}
-        render={({ field }) => <input placeholder="Doświadczenie" type={"number"} {...field} />}
-      />
-      <Controller
-        name="aboutMe"
-        control={control}
-        render={({ field }) => <input placeholder="O mnie..." type={"text"} {...field} />}
-      />
-      <Controller
-        name="services"
-        control={control}
-        render={({ field }) => <input placeholder="Usługi" type={"text"} {...field} />}
-      />
-      <Controller
-        name="gallery"
-        control={control}
-        render={({ field }) => <input placeholder="Galeria" type={"text"} {...field} />}
-      />
+      <ProfileInputFreelancerStyled>
+        <Controller
+          name="firstName"
+          control={control}
+          render={({ field }) => <input placeholder="Imię" type={"text"} {...field} />}
+        />
+        <Controller
+          name="secondName"
+          control={control}
+          render={({ field }) => <input placeholder="Nazwisko" type={"text"} {...field} />}
+        />
+        <Controller
+          name="email"
+          control={control}
+          render={({ field }) => <input placeholder="Email" type={"email"} {...field} />}
+        />
+        <Controller
+          name="country"
+          control={control}
+          render={({ field }) => <input placeholder="Kraj" type={"text"} {...field} />}
+        />
+        <Controller
+          name="city"
+          control={control}
+          render={({ field }) => <input placeholder="Miasto" type={"text"} {...field} />}
+        />
+        <Controller
+          name="experience"
+          control={control}
+          render={({ field }) => <input placeholder="Doświadczenie" type={"number"} {...field} />}
+        />
+        <Controller
+          name="aboutMe"
+          control={control}
+          render={({ field }) => <AboutMeStyled placeholder="O mnie..." type={"text"} {...field} />}
+        />
+        <Controller
+          name="services"
+          control={control}
+          render={({ field }) => <input placeholder="Usługi" type={"text"} {...field} />}
+        />
+        <Controller
+          name="gallery"
+          control={control}
+          render={({ field }) => <input placeholder="Galeria" type={"text"} {...field} />}
+        />
 
-      <button>Send</button>
+        <button>Send</button>
+      </ProfileInputFreelancerStyled>
     </>
   );
 };
