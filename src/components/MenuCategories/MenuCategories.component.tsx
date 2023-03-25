b85c5bcd3b642555fecefb9acc00f7523b903494
Marta Zielińska <marta.zielinska1987@gmail.com>
2023-03-25 11:36:35 +0100

    Padding and gap styling added in MenuCategories.styled.ts

--- src/components/MenuCategories/MenuCategories.component.tsx
+++ src/components/MenuCategories/MenuCategories.component.tsx
@@ -1,12 +1,15 @@
+import { SecondaryButton } from "../UI/Buttons/Buttons.styled";
+import { MenuCategoriesButtons } from "./MenuCategories.styled";
+
 export const MenuCategories = () => {
   return (
-    <div>
-      <button>Branding</button>
-      <button>Druk</button>
-      <button>Digital</button>
-      <button>UX/UI</button>
-      <button>Ilustracje</button>
-      <button>Inne</button>
-    </div>
+    <MenuCategoriesButtons>
+      <SecondaryButton>Branding</SecondaryButton>
+      <SecondaryButton>Druk</SecondaryButton>
+      <SecondaryButton>Digital</SecondaryButton>
+      <SecondaryButton>UX/UI</SecondaryButton>
+      <SecondaryButton>Ilustracje</SecondaryButton>
+      <SecondaryButton>Inne</SecondaryButton>
+    </MenuCategoriesButtons>
   );
 };
---
+++ src/components/MenuCategories/MenuCategories.styled.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const MenuCategoriesButtons = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+
+  gap: 20px;
+  padding: 45px;
+`;
