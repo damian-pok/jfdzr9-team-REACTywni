37ecf320457b428415dfa20b3cd41ae9270c266d
Katarzyna Chmielewska <katarzynachmielewska@int.pl>
2023-03-26 11:21:10 +0200

    added simple animation

--- src/components/LogoCarousel/LogoCarousel.styled.ts
+++ src/components/LogoCarousel/LogoCarousel.styled.ts
@@ -1,16 +1,24 @@
 import styled from "styled-components";
 
 export const LogoField = styled.div`
+  width: calc(250px * 14);
   display: flex;
   flex-direction: row;
   justify-content: center;
   align-self: center;
   gap: 5rem;
   padding: 4rem;
+  overflow: hidden;
+  position: relative;
 `;
 
 export const LogoStyles = styled.img`
   height: 2rem;
   width: auto;
   gap: 1.5rem;
+  opacity: 0.3;
+  :hover {
+    transform: scale(1.2);
+    opacity: 1;
+  }
 `;
