3415e51e3ce5763e4438bfd6fd35ea2c964af3b5
Marta Zielińska <marta.zielinska1987@gmail.com>
2023-03-26 09:13:36 +0200

    In DropDownLists.component.tsx function to show up the drop down list added

--- src/components/DropDownLists/DropDownElements.ts
+++ src/components/DropDownLists/DropDownElements.ts
@@ -2,19 +2,19 @@ export const dropDownBranding = [
   {
     id: 1,
     title: "logo",
-    path: "./logo",
+    path: "./branding/logo",
   },
 
   {
     id: 2,
     title: "księga znaków",
-    path: "./ksiegaznakow",
+    path: "./branding/ksiegaznakow",
   },
 
   {
     id: 3,
     title: "identyfikacja wizualna",
-    path: "./identyfikacjawizualna",
+    path: "./branding/identyfikacjawizualna",
   },
 ];
 
@@ -22,19 +22,19 @@ export const dropDownDruk = [
   {
     id: 1,
     title: "opcja1",
-    path: "./opcja1",
+    path: "./druk/opcja1",
   },
 
   {
     id: 2,
     title: "opcja2",
-    path: "./opcja2",
+    path: "./druk/opcja2",
   },
 
   {
     id: 3,
     title: "opcja3",
-    path: "./opcja3",
+    path: "./druk/opcja3",
   },
 ];
 
@@ -42,19 +42,19 @@ export const dropDownDigital = [
   {
     id: 1,
     title: "opcja1",
-    path: "./opcja1",
+    path: "./digital/opcja1",
   },
 
   {
     id: 2,
     title: "opcja2",
-    path: "./opcja2",
+    path: "./digital/opcja2",
   },
 
   {
     id: 3,
     title: "opcja3",
-    path: "./opcja3",
+    path: "./digital/opcja3",
   },
 ];
 
@@ -62,19 +62,19 @@ export const dropDownUXUI = [
   {
     id: 1,
     title: "opcja1",
-    path: "./opcja1",
+    path: "./ux-ui/opcja1",
   },
 
   {
     id: 2,
     title: "opcja2",
-    path: "./opcja2",
+    path: "./ux-ui/opcja2",
   },
 
   {
     id: 3,
     title: "opcja3",
-    path: "./opcja3",
+    path: "./ux-ui/opcja3",
   },
 ];
 
@@ -82,18 +82,18 @@ export const dropDownIlustracje = [
   {
     id: 1,
     title: "opcja1",
-    path: "./opcja1",
+    path: "./ilustracje/opcja1",
   },
 
   {
     id: 2,
     title: "opcja2",
-    path: "./opcja2",
+    path: "./ilustracje/opcja2",
   },
 
   {
     id: 3,
     title: "opcja3",
-    path: "./opcja3",
+    path: "./ilustracje/opcja3",
   },
 ];
--- src/components/DropDownLists/DropDownLists.component.tsx
+++ src/components/DropDownLists/DropDownLists.component.tsx
@@ -1,12 +1,23 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import { DetailsList } from "./DropDownLists.styled";
 import { ListElement } from "./DropDownLists.styled";
+import { dropDownBranding } from "./DropDownElements";
 
 export const DropDownList = () => {
+  const [dropdown, setDropdown] = useState(false);
+
   return (
-    <DetailsList>
-      <ListElement>logo</ListElement>
-      <ListElement>księga znaków</ListElement>
-      <ListElement>identyfikacja wizualna</ListElement>
+    <DetailsList onClick={() => setDropdown(!dropdown)}>
+      {dropDownBranding.map((item) => {
+        return (
+          <ListElement key={item.id}>
+            <Link to={item.path} onClick={() => setDropdown(false)}>
+              {item.title}
+            </Link>
+          </ListElement>
+        );
+      })}
     </DetailsList>
   );
 };
