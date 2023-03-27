ff658939b718adda075d468e42cfe9ee3e7f627c
Marta Zielińska <marta.zielinska1987@gmail.com>
2023-03-27 20:52:14 +0200

    Component HowItWorksDescription with basic styling created and imported to HowClient.page. <h2> header removed from HowClient.page

--- src/pages/HowClient.page.tsx
+++ src/pages/HowClient.page.tsx
@@ -1,9 +1,11 @@
 import { MenuCategories } from "../components/MenuCategories/MenuCategories.component";
+import HowItWorksDescription from "../components/HowItWorksDescription/HowItWorksDescription.component";
 
 const HowClient = () => {
   return (
     <>
       <h1>How it works - for Client!</h1>
+      <HowItWorksDescription />
       <MenuCategories />
     </>
   );
