b107af978b4e29b17d180e96be3a9146559d9926
Marta Zielińska <marta.zielinska1987@gmail.com>
2023-03-27 21:16:04 +0200

    Component Plans created and imported to HowClient.page

---
+++ src/components/Plans/Plans.component.tsx
@@ -0,0 +1,16 @@
+import { PlansArea } from "./Plans.styled";
+
+export const Plans = () => {
+  return (
+    <PlansArea>
+      <h2>Our plans</h2>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa tempore consequatur consectetur voluptas
+        exercitationem dolore? Rem odit excepturi deserunt distinctio sequi hic aliquam. Cumque deserunt officia ad
+        excepturi, suscipit repellat?
+      </p>
+    </PlansArea>
+  );
+};
+
+export default Plans;
---
+++ src/components/Plans/Plans.styled.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const PlansArea = styled.div`
+  height: fit-content;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  padding: 4rem;
+`;
