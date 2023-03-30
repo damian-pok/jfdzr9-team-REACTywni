75431368c3a850b187a2c4dfb5d183bcd17a759c
Marta Zielińska <marta.zielinska1987@gmail.com>
2023-03-30 22:21:00 +0200

    Container for answer created and styled. Temporarily added to HowClient page

---
+++ src/components/Answer/Answer.styled.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const AnswerArea = styled.div`
+  height: fit-content;
+  width: auto;
+  display: flex;
+  gap: 1.7rem;
+  align-self: flex-start;
+  align-items: center;
+  padding: 2rem;
+  margin-left: 3.7rem;
+`;
--- src/components/FAQ/FAQ.component.tsx
+++ src/components/FAQ/FAQ.component.tsx
@@ -1,11 +1,13 @@
 import { FAQArea } from "./FAQ.styled";
 import { SingleQuestion } from "../SingleQuestion/SingleQuestion.component";
+import { Answer } from "../Answer/Answer.component";
 
 export const FAQ = () => {
   return (
     <FAQArea>
       <h2>Frequently Asked Questions</h2>
       <SingleQuestion />
+      <Answer />
       <SingleQuestion />
       <SingleQuestion />
       <SingleQuestion />
