5441edaed2a6de26250c906b3bf51ecc2ff60562
Katarzyna Chmielewska <katarzynachmielewska@int.pl>
2023-04-01 19:50:31 +0200

    added some style to login site

--- src/components/UI/Input/Input.styled.ts
+++ src/components/UI/Input/Input.styled.ts
@@ -11,3 +11,15 @@ export const InputForm = styled.input`
     color: var(--light-grey-color);
   }
 `;
+
+export const InputLoginSign = styled.input`
+  width: 20rem;
+  height: 2.5rem;
+  background-color: var(--white-color);
+  border: 2px solid var(--black);
+  border-radius: 0.5rem;
+  padding-left: 1rem;
+  ::placeholder {
+    color: var(--light-grey-color);
+  }
+`;
