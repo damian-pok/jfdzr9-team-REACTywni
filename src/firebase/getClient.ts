6d4888690aecd171a2a0f9bfc142bc92c1b65987
Damian Pokrop <d.pokrop@gmail.com>
2023-04-01 11:58:57 +0200

    firebase fixed

--- src/firebase/getClient.ts
+++ src/firebase/getClient.ts
@@ -3,8 +3,16 @@ import { doc, getDoc } from "firebase/firestore";
 import { db, auth } from "../firebase/firebase.config";
 
 export const getClient = async () => {
-  const docRef = doc(db, "client", String(auth.currentUser.uid));
+  const userId = auth.currentUser ? auth.currentUser.uid : null;
+  const docRef = doc(db, "client", String(userId));
   const docSnap = await getDoc(docRef);
 
   return { docSnap };
 };
+
+// export const getClient = async () => {
+//   const docRef = doc(db, "client", String(auth.currentUser.uid));
+//   const docSnap = await getDoc(docRef);
+
+//   return { docSnap };
+// };
--- src/firebase/getFreelancer.ts
+++ src/firebase/getFreelancer.ts
@@ -3,8 +3,16 @@ import { doc, getDoc } from "firebase/firestore";
 import { db, auth } from "../firebase/firebase.config";
 
 export const getFreelancer = async () => {
-  const docRef = doc(db, "freelancer", String(auth.currentUser.uid));
+  const userId = auth.currentUser ? auth.currentUser.uid : null;
+  const docRef = doc(db, "freelancer", String(userId));
   const docSnapF = await getDoc(docRef);
 
   return { docSnapF };
 };
+
+// export const getFreelancer = async () => {
+//   const docRef = doc(db, "freelancer", String(auth.currentUser.uid));
+//   const docSnapF = await getDoc(docRef);
+
+//   return { docSnapF };
+// };
