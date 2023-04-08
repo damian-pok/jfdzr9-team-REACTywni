5ec4c0866ad1dbaf5dac4b87406c31f13b0dc644
Marta Zielińska <marta.zielinska1987@gmail.com>
2023-04-08 17:58:39 +0200

    Styling of container SingleDesignerWidget has been adjusted

--- src/components/SingleDesignerWidget/SingleDesignerWidget.component.tsx
+++ src/components/SingleDesignerWidget/SingleDesignerWidget.component.tsx
@@ -1,29 +1,22 @@
-import {
-  DesignerWidget,
-  DesignerPhoto,
-  DesignerTags,
-  DesignerContent,
-  Tag,
-  PhotoContainer,
-} from "./SingleDesignerWidget.styled";
+import { DesignerWidget, DesignerPhoto, DesignerTags, DesignerContent, Tag } from "./SingleDesignerWidget.styled";
 import designer from "../../../public/designer.jpg";
 
 export const SingleDesignerWidget = () => {
   return (
     <DesignerWidget>
-      <PhotoContainer>
-        <DesignerPhoto src={designer} />
-        <DesignerTags>
-          <Tag>branding</Tag>
-          <Tag>UX/UI</Tag>
-          <Tag>inne</Tag>
-        </DesignerTags>
-      </PhotoContainer>
+      <DesignerPhoto src={designer} />
       <DesignerContent>
         <h3>Anna Karenina</h3>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus a incidunt id ab, sint porro nihil quaerat ex
         nulla totam voluptas laudantium fuga atque iste ad ducimus minima! Incidunt, quis.
       </DesignerContent>
+      <DesignerTags>
+        <Tag>branding</Tag>
+        <Tag>UX/UI</Tag>
+        <Tag>digital</Tag>
+        <Tag>druk</Tag>
+        <Tag>inne</Tag>
+      </DesignerTags>
     </DesignerWidget>
   );
 };
--- src/components/SingleDesignerWidget/SingleDesignerWidget.styled.ts
+++ src/components/SingleDesignerWidget/SingleDesignerWidget.styled.ts
@@ -1,31 +1,32 @@
 import styled from "styled-components";
 
 export const DesignerWidget = styled.div`
-  height: 225px;
-  display: flex;
-  align-items: flex-start;
+  height: 300px;
+  display: grid;
+  grid-template-columns: 1fr 5fr;
+  grid-template-rows: 2fr 1fr;
+  gap: 1.8rem;
   border-radius: 5px;
   background-color: var(--lime-yellow);
   padding: 3.5rem;
 `;
 
 export const DesignerPhoto = styled.img`
-  height: 200px;
-  width: 165px;
+  height: auto;
+  width: 179px;
   border-radius: 5px;
 `;
 
 export const DesignerTags = styled.div`
   display: flex;
-  justify-content: flex-end;
-  margin-top: auto;
   gap: 0.7rem;
-  height: fit-content;
+  height: auto;
   width: fit-content;
+  grid-column: 1 / span 2;
 `;
 
 export const Tag = styled.p`
-  height: auto;
+  height: fit-content;
   color: var(--white-color);
   font-size: bold;
   background-color: var(--black);
@@ -34,18 +35,10 @@ export const Tag = styled.p`
 `;
 
 export const DesignerContent = styled.p`
-  height: fit-content;
+  height: auto;
   border-radius: 5px;
   padding: 1.7rem;
   display: flex;
   flex-direction: column;
-  flex-grow: 1;
   background-color: var(--white-color);
-`;
-
-export const PhotoContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  gap: 0.85rem;
 `;
