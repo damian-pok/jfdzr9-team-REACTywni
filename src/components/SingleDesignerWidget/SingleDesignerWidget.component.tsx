1d8be71a3621f18f63b464f6a10f96bfac4feb19
Marta Zielińska <marta.zielinska1987@gmail.com>
2023-04-13 14:14:54 +0200

    Section for second category of tags has been added to SingleDesignerWidget. Search page layout has been finished

--- src/components/SingleDesignerWidget/SingleDesignerWidget.component.tsx
+++ src/components/SingleDesignerWidget/SingleDesignerWidget.component.tsx
@@ -1,4 +1,13 @@
-import { DesignerWidget, DesignerPhoto, DesignerTags, DesignerContent, Tag } from "./SingleDesignerWidget.styled";
+import {
+  DesignerWidget,
+  DesignerPhoto,
+  DesignerTags,
+  DesignerContent,
+  Tag,
+  DesignerTagsSecond,
+  TagSecond,
+  TagsWrapper,
+} from "./SingleDesignerWidget.styled";
 import designer from "../../../public/designer.jpg";
 import designer3 from "../../../public/designer3.jpg";
 
@@ -13,10 +22,18 @@ export const SingleDesignerWidget = () => {
           pracuję już od kilku lat, moją specjalnością jest projektowanie identyfikacji wizualnej oraz interfejsów
           użytkownika.
         </DesignerContent>
-        <DesignerTags>
-          <Tag>branding</Tag>
-          <Tag>UX/UI</Tag>
-        </DesignerTags>
+        <TagsWrapper>
+          <DesignerTags>
+            <Tag>branding</Tag>
+            <Tag>UX/UI</Tag>
+          </DesignerTags>
+          <DesignerTagsSecond>
+            <TagSecond>ulotki</TagSecond>
+            <TagSecond>plakaty</TagSecond>
+            <TagSecond>photoshop</TagSecond>
+            <TagSecond>figma</TagSecond>
+          </DesignerTagsSecond>
+        </TagsWrapper>
       </DesignerWidget>
 
       <DesignerWidget>
@@ -26,11 +43,20 @@ export const SingleDesignerWidget = () => {
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi similique voluptatibus recusandae. Tempore hic
           quasi eos pariatur id numquam officiis vel dolores culpa. Odit quo vitae ipsam dolorum ipsa eius!
         </DesignerContent>
-        <DesignerTags>
-          <Tag>UX/UI</Tag>
-          <Tag>druk</Tag>
-          <Tag>inne</Tag>
-        </DesignerTags>
+        <TagsWrapper>
+          <DesignerTags>
+            <Tag>UX/UI</Tag>
+            <Tag>druk</Tag>
+            <Tag>inne</Tag>
+          </DesignerTags>
+          <DesignerTagsSecond>
+            <TagSecond>ux</TagSecond>
+            <TagSecond>ui</TagSecond>
+            <TagSecond>figma</TagSecond>
+            <TagSecond>plakaty</TagSecond>
+            <TagSecond>ulotki</TagSecond>
+          </DesignerTagsSecond>
+        </TagsWrapper>
       </DesignerWidget>
     </>
   );
--- src/components/SingleDesignerWidget/SingleDesignerWidget.styled.ts
+++ src/components/SingleDesignerWidget/SingleDesignerWidget.styled.ts
@@ -3,8 +3,9 @@ import styled from "styled-components";
 export const DesignerWidget = styled.div`
   display: grid;
   grid-template-columns: 1fr 5fr;
-  /* grid-template-rows: 2fr 1fr; */
-  gap: 1.8rem;
+  grid-template-rows: 2fr 1fr;
+  column-gap: 1.8rem;
+  row-gap: 0.7rem;
   border-radius: 15px;
   background-color: var(--lime-yellow);
   padding: 20px 50px 34px 20px;
@@ -18,7 +19,32 @@ export const DesignerPhoto = styled.img`
   object-fit: cover;
 `;
 
+export const TagsWrapper = styled.div`
+  display: grid;
+  grid-column: 1 / span 2;
+`;
+
 export const DesignerTags = styled.div`
+  display: flex;
+  column-gap: 0.7rem;
+  margin-bottom: 0.7rem;
+  height: auto;
+  width: fit-content;
+  grid-column: 1 / span 2;
+`;
+
+export const Tag = styled.p`
+  height: fit-content;
+  width: 106px;
+  color: var(--white-color);
+  font-size: bold;
+  background-color: var(--black);
+  border-radius: 5px;
+  padding: 0.4rem 1rem 0.4rem 1rem;
+  text-align: center;
+`;
+
+export const DesignerTagsSecond = styled.div`
   display: flex;
   gap: 0.7rem;
   height: 80px;
@@ -26,13 +52,16 @@ export const DesignerTags = styled.div`
   grid-column: 1 / span 2;
 `;
 
-export const Tag = styled.p`
+export const TagSecond = styled.p`
   height: fit-content;
-  color: var(--white-color);
+  width: 106px;
+  color: var(--black);
   font-size: bold;
-  background-color: var(--black);
+  background-color: var(--very-light-grey-color);
+  border: 1px solid var(--dark-grey-color);
   border-radius: 5px;
   padding: 0.4rem 1rem 0.4rem 1rem;
+  text-align: center;
 `;
 
 export const DesignerContent = styled.p`
